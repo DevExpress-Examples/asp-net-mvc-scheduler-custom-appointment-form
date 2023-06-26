ccb7ab3b208940c2a845b81695824b479da5eb41
DevExpressExampleBot <fakeEmail>
2023-06-26 13:28:38 +0400

    Source auto update [skip ci]

--- VB/Properties/AssemblyInfo.vb
+++ VB/Properties/AssemblyInfo.vb
@@ -1,4 +1,28 @@
 ' Developer Express Code Central Example:
+' Scheduler - How to implement a custom Edit Appointment Form with custom fields
+' 
+' This example illustrates how to implement a custom Appointment Form and display
+' it instead of the default one.
+' 
+' To include a custom Appointment Form to the
+' SchedulerPartial view, the
+' MVCxSchedulerOptionsForms.SetAppointmentFormTemplateContent Method
+' (ms-help://DevExpress.NETv12.2/DevExpress.AspNet/DevExpressWebMvcMVCxSchedulerOptionsForms_SetAppointmentFormTemplateContenttopic.htm)
+' should be handled.
+' To add custom fields to the Appointment Form, implement a
+' custom AppointmentFormTemplateContainer
+' (ms-help://DevExpress.NETv12.2/DevExpress.AspNet/clsDevExpressWebASPxSchedulerAppointmentFormTemplateContainertopic.htm)
+' and substitute the default container with your custom one by handling the
+' ASPxScheduler.AppointmentFormShowing Event
+' (ms-help://DevExpress.NETv12.2/DevExpress.AspNet/DevExpressWebASPxSchedulerASPxScheduler_AppointmentFormShowingtopic.htm).
+' See
+' Also:
+' http://www.devexpress.com/scid=E2924
+' http://www.devexpress.com/scid=E3984
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4520
+' Developer Express Code Central Example:
 ' Scheduler - Getting Started - Lesson 2 - Implement Insert-Update-Delete appointment functionality
 ' 
 ' This example demonstrates a simple application that enables end-users to add new
@@ -13,33 +37,26 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E3984
-
-
-Imports Microsoft.VisualBasic
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("DevExpressMvcApplication1")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("DevExpressMvcApplication1")>
-<Assembly: AssemblyCopyright("Copyright ? 2012")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("DevExpressMvcApplication1")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("DevExpressMvcApplication1")>
+<Assembly:AssemblyCopyright("Copyright ? 2012")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("9fe9ebbc-b32e-4e40-b81a-a3e219e9e91f")>
-
+<Assembly:Guid("9fe9ebbc-b32e-4e40-b81a-a3e219e9e91f")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -49,5 +66,5 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Revision and Build Numbers 
 ' by using the '*' as shown below:
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
